e53d47d357155fe7f2926420329012b4787d3f14
Franklyn Dunbar <franklyn.dunbar@earthscope.org>
2024-08-16 15:38:39 -0800

    get build paths for raytracing

--- garpos/__init__.py
+++ garpos/__init__.py
@@ -1,1 +1,10 @@
+from pathlib import Path
+
 from .garpos_v102.garpos_main import drive_garpos
+
+for path in Path(__file__).parent.rglob('*'):
+    if path.is_dir() and path.name == 'f90lib':
+        print(path)
+        LIB_DIRECTORY = str(path)
+
+LIB_RAYTRACE = 'lib_raytrace.so'
